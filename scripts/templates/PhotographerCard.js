dc0cb44c426f30502c4d082defab014694d54e23
Nicolas <nicolas.mazaud.sio@gmail.com>
2023-01-12 21:15:29 +0100

    Style page d'acceuil

--- scripts/templates/PhotographerCard.js
+++ scripts/templates/PhotographerCard.js
@@ -7,18 +7,18 @@ export default class PhotographerCard {
     const $section = document.createElement("article");
 
     const photographerCard = `
-        <a href="photographer.html?id=${this._photographer.id}">
+        <a href="photographer.html?id=${this._photographer.id}" class="photographer">
             <img
                 alt="${this._photographer.name}"
                 src="/${this._photographer.portrait}"
                 class="photographer-img"
             />
-            <h3>${this._photographer.name}</h3>
+            <h2 class="photographer-name">${this._photographer.name}</h2>
         </a>
-        <p>
-            <span>${this._photographer.city}, ${this._photographer.country}</span>
-            <span>${this._photographer.tagline}</span>
-            <span>${this._photographer.price}€/jour</span>
+        <p class="photographer-details">
+            <span class="photographer-location">${this._photographer.city}, ${this._photographer.country}</span>
+            <span class="photographer-tagline">${this._photographer.tagline}</span>
+            <span class="photographer-price">${this._photographer.price}€/jour</span>
         </p>
     `;
 
